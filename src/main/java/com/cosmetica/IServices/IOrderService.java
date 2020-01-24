47f28249fe31908ca9d87aadb32feeceab2cfbbc
abdeladimabid <abdeladim.abid@gmail.com>
2020-01-24 11:16:53 +0100

    edited I services added methods

--- src/main/java/com/cosmetica/IServices/IOrderService.java
+++ src/main/java/com/cosmetica/IServices/IOrderService.java
@@ -3,7 +3,10 @@ package com.cosmetica.IServices;
 import java.util.List;
 import java.util.Optional;
 
+import com.cosmetica.Entities.Cart;
 import com.cosmetica.Entities.Order;
+import com.cosmetica.Entities.Product;
+import com.cosmetica.Entities.User;
 
 public interface IOrderService {
 	
@@ -15,5 +18,10 @@ public interface IOrderService {
 
 	public void delete(Order order);
 
+	public User getOrderUser(Order Order);
+	
+	public Product getOrderProduct(Order Order);
+	
+	public Cart getOrderCart(Order Order) ;
 
 }
