97fba104023a1f79b620bc7c35fdef226503d696
abdeladimabid <abdeladim.abid@gmail.com>
2020-01-25 16:27:53 +0100

    User controller

---
+++ src/main/java/com/cosmetica/Controllers/UserController.java
@@ -0,0 +1,56 @@
+package com.cosmetica.Controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cosmetica.Entities.User;
+import com.cosmetica.Exceptions.CosmeticaException;
+import com.cosmetica.IServices.IUserService;
+
+@RestController
+@RequestMapping("COSMETICA")
+public class UserController {
+	@Autowired
+	IUserService userservice;
+	
+	 @GetMapping("/users")
+	 public List<User> allUsers() {
+		List<User> users = userservice.getAll();
+		return users;
+		 
+	 }
+	 
+	 @GetMapping("/user/{user_id}")
+	 public Optional <User> oneUser(@PathVariable("user_id")int user_id){
+		 
+		 if(!userservice.getOneById(user_id).isPresent())
+	         throw new CosmeticaException(user_id );
+		 return userservice.getOneById(user_id);
+		 
+	 }
+
+	 @PostMapping("/user")
+	 public void addUser(@RequestBody User user) {
+		 userservice.saveOrUpdate(user);
+		 
+	 }
+	 @DeleteMapping("/remove/{user_id}")
+	 public void removeUser(@PathVariable("user_id")int user_id) {
+		 if(!userservice.getOneById(user_id).isPresent())
+	         throw new CosmeticaException(user_id );
+		 User user = userservice.getOneById(user_id).get();
+		 userservice.delete(user); 
+		 
+	 }
+	 
+
+}
