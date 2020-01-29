be7d054ddcd1a16568b885966ffaf552aa1ecbac
abdeladimabid <abdeladim.abid@gmail.com>
2020-01-29 14:25:36 +0100

    Fixing issues

--- src/main/java/com/cosmetica/DAO/IAdminDao.java
+++ src/main/java/com/cosmetica/DAO/IAdminDao.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Repository;
 
 import com.cosmetica.Entities.Admin;
 
+
 @Repository
 public interface IAdminDao  extends JpaRepository<Admin , Integer>{
 	
 	Optional<Admin> findByUsernameOrEmail(String firstname, String lastname);
-	List<Admin> findByFirstnameOrLastnameContaining(String firstname, String lastname);
+	List<Admin> findByLastnameContaining(String lastname);
+	List<Admin> findByFirstnameContaining(String firstname);
 	List<Admin> findByUsernameOrEmailContaining(String username,String email);
 	List<Admin> findByUsernameContaining(String username);
 	Optional<Admin> findByEmail(String email);
