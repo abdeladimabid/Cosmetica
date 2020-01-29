3bfd0281561df8d4421b489d53e78cdfc5177511
abdeladimabid <abdeladim.abid@gmail.com>
2020-01-29 20:43:55 +0100

    not finished yet

--- src/main/java/com/cosmetica/Controllers/InvoiceBodyController.java
+++ src/main/java/com/cosmetica/Controllers/InvoiceBodyController.java
@@ -28,14 +28,21 @@ public class InvoiceBodyController {
 	@Autowired
 	InvoiceBodyService invoicebodyservice;
 	
-	@GetMapping("/invoicebodys")							//get all invoiceBodys
+	  /*InvoiceBody is basically the recipient group of infos : 
+	  **Receiving date, Recipient first name and last name, Recipient phone and address.
+	  **Since those infos doesn't change regardless the type of invoice we want
+	  **We store them in a separate record, then link them to different Heads
+	  **Based on the type of invoice the user needs. */
+	
+	
+	@GetMapping("/bodys")						//get all invoiceBodys
 	 public List<InvoiceBody> allinvoicebodys() {
 		List<InvoiceBody> invoicebodys = invoicebodyservice.getAll();
 		return invoicebodys;
 		 
 	 }
 	 
-	 @GetMapping("/invoicebody/{invoicebody_id}")				//search a invoiceBody by invoicebody_Id, takes a invoicebody_Id in parameters
+	 @GetMapping("/body/{invoicebody_id}")		//search a invoiceBody by invoicebody_Id, takes a invoicebody_Id in parameters
 	 public Optional <InvoiceBody> oneinvoicebody(@PathVariable("invoicebody_id")int invoicebody_id){
 		 
 		 if(!invoicebodyservice.getOneById(invoicebody_id).isPresent())
@@ -44,18 +51,18 @@ public class InvoiceBodyController {
 		 
 	 }
 
-	 @PostMapping("/add/invoicebody")						//add a new invoiceBody , takes an invoiceBody in parameters
+	 @PostMapping("/add/invoicebody")					//add recipient info , takes an invoiceBody in parameters
 	 public void addinvoicebody(@RequestBody InvoiceBody invoicebody) {
 		 invoicebodyservice.saveOrUpdate(invoicebody);
 		 
 	 }
-	 @PutMapping("/modify/invoicebody")					//modify a invoiceBody , takes the new invoiceBody in parameters
+	 @PutMapping("/modify/invoicebody")					//modify recipient info , takes the new invoiceBody in parameters
 	 public void modifyinvoicebody(@RequestBody InvoiceBody invoicebody) {
 		 invoicebodyservice.saveOrUpdate(invoicebody);
 		 
 	 }
 	 
-	 @DeleteMapping("/invoicebody/remove/{invoicebody_id}")		//remove a invoiceBody , takes an invoicebody_Id in parameters
+	 @DeleteMapping("/body/remove/{invoicebody_id}")		//remove a invoiceBody , takes an invoicebody_Id in parameters
 	 public void removeinvoicebody(@PathVariable("invoicebody_id")int invoicebody_id) {
 		 if(!invoicebodyservice.getOneById(invoicebody_id).isPresent())
 	         throw new CosmeticaException(invoicebody_id );
@@ -64,7 +71,7 @@ public class InvoiceBodyController {
 		 
 	 }
 	 
-	 @GetMapping("/invoicebody/heads/{invoicebody_id}")	//get all invoiceHeads, takes an invoicebody_Id in parameters
+	 @GetMapping("/body/heads/{invoicebody_id}")	//get all invoiceHeads, takes an invoicebody_Id in parameters
 	 public List<InvoiceHead> invoicebodyheads(@PathVariable("invoicebody_id")int invoicebody_id) {
 		 if(!invoicebodyservice.getOneById(invoicebody_id).isPresent())
 	         throw new CosmeticaException(invoicebody_id );
@@ -72,7 +79,7 @@ public class InvoiceBodyController {
 		 return invoicebodyservice.getBodyHeads(invoicebody); 
 	 }
 	 
-	 @GetMapping("/invoicebody/heads/{invoicebody_id}")	//get invoiceBody Cart, takes an invoicebody_Id in parameters
+	 @GetMapping("/body/cart/{invoicebody_id}")	//get invoiceBody Cart, takes an invoicebody_Id in parameters
 	 public Cart invoiceBodyCart(@PathVariable("invoicebody_id")int invoicebody_id) {
 		 if(!invoicebodyservice.getOneById(invoicebody_id).isPresent())
 	         throw new CosmeticaException(invoicebody_id );
