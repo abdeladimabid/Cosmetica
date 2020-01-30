2927393aede5863de48b9de73ced5af627c44ffe
Leila M'HAIDRAT <44430242+Leila-Mhaidrat@users.noreply.github.com>
2020-01-30 09:28:43 +0100

    Update AuthentificationResponse.java

--- src/main/java/com/cosmetica/Authentication/AuthentificationResponse.java
+++ src/main/java/com/cosmetica/Authentication/AuthentificationResponse.java
@@ -1,5 +1,17 @@
 package com.cosmetica.Authentication;
 
+
+
 public class AuthentificationResponse {
 
+	private final String jwt;
+
+	public String getJwt() {
+		return jwt;
+	}
+
+	public AuthentificationResponse(String jwt) {
+		this.jwt = jwt;
+	}
 }
+
